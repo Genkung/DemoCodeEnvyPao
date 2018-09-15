69c3d2903a60f44c75dae9ede597afa4e342693e
Parada <Parada@perfenterprise.com>
2018-09-15 10:04:22 +0000

    implement ionic

--- Quize/API/Startup.cs
+++ Quize/API/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,12 +16,18 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddCors();
+            services.AddMvc();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+                builder.WithOrigins("*")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
             app.UseMvc();
         }
     }
--- Quize/Mobile/src/pages/home/home.ts
+++ Quize/Mobile/src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ sumnumber:any;
+ numberresual:any;
+  constructor(public navCtrl: NavController,private http: HttpClient) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  
+ click() {
+    this.http.get<number[]>("http://node15.codenvy.io:49250/api/Consecutive/CalculateFromSum/"+this.sumnumber)
+      .subscribe((data) => {
+this.numberresual = data;
+    console.log("Resual : " + data);
+      },
+        error => {
+          alert("Error: " + error + "\nError message: " + error.message + "\nError result: " + error.error)
+        });
   }
 
 
